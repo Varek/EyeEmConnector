bbf21142c6737520ff2a89b4406e78ec68c2a963
Varek <4ndr3r13ck@googlemail.com>
2015-11-28 20:01:39 +0100

    update api host

--- lib/EyeEmConnector/configuration.rb
+++ lib/EyeEmConnector/configuration.rb
@@ -2,7 +2,7 @@ module EyeEmConnector
 
   module Configuration
 
-    ENDPOINT = 'https://www.eyeem.com/api/v2/'
+    ENDPOINT = 'https://api.eyeem.com/v2/'
     VALID_OPTIONS = [:client_id, :client_secret, :access_token, :api_version]
     attr_accessor *VALID_OPTIONS
 
